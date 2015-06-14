00ffef616e738ee85df7db3f95f8597eed58b7a8
Martín González <m@martingonzalez.net>
2015-06-15 01:13:12 +0200

    add huge warped png, simplify js

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,15 @@
-// Image overlay
-var imageUrl = 'img/canal-infanta.jpg',
+// FIXME: need to adjust coordinates for not stretching the img
+var imageUrl = 'img/canal-warp.png',
     imageBounds = L.latLngBounds([
         [41.327874, 1.980321],
         [41.393679, 2.186463]]);
 
-map = new L.Map('map', {
-  zoomControl: false,
-  fitBounds: imageBounds,
-}).setView([41.358670, 2.087876], 13);
+var map = new L.Map('map', {
+  zoomControl: false
+}).fitBounds(imageBounds);
 
 var overlay = L.imageOverlay(imageUrl, imageBounds, {opacity: 0.4})
-  .addTo(map);
+    .addTo(map);
 
 // Disable drag and zoom handlers.
 map.dragging.disable();
@@ -27,8 +26,8 @@ document.getElementById('map').style.cursor='default';
 // retina detection & tiles
 var attribution = {attribution: 'Map tiles by <a href="http://cartodb.com/attributions#basemaps">CartoDB</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="http://www.openstreetmap.org/">OpenStreetMap</a>, under ODbL.'};
 var retina = window.devicePixelRatio > 1;
-if (retina) {
-  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png',attribution).addTo(map);
-} else {
-  L.t
+  if (retina) {
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png',attribution).addTo(map);
+  } else {
+    L.t
   }
