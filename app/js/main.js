e1e57fb2adc9dfa5428aa0ab16173c0cfebd335d
Martín González <m@martingonzalez.net>
2015-06-15 15:13:48 +0200

    Change transparency

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ var map = L.map('map', {
     layers:[
       L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png'),
       L.tileLayer('http://mapwarper.net/maps/tile/9810/{z}/{x}/{y}.png', {
-        opacity: 0.2,
+        opacity: 0.4,
         attribution: 'Map tiles by <a href="http://cartodb.com/attributions#basemaps">CartoDB</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="http://www.openstreetmap.org/">OpenStreetMap</a>, under ODbL. | <a href="http://cartotecadigital.icc.cat/cdm/ref/collection/catalunya/id/1602">Institut Cartogràfic i Geològic de Catalunya</a>.'
       })
     ]
