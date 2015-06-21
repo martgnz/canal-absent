53b79f07168e9914a966d19d49d16e262fe8c97b
Martín González <m@martingonzalez.net>
2015-06-21 17:33:47 +0200

    A round of improvements: remove awesome markers and use the maki set. Add the markers descriptions

--- app/js/main.js
+++ app/js/main.js
@@ -25,67 +25,101 @@ var map = L.map('map', {
 map.scrollWheelZoom.disable();
 
 // Awesome markers
-var marker1 = L.AwesomeMarkers.icon({
-    icon: 'university',
-    markerColor: 'blue'
+var marker1 = L.MakiMarkers.icon({
+    icon: 'water',
+    color: '#0074D9',
+    size: 'l'
 });
 
-var marker2 = L.AwesomeMarkers.icon({
-    icon: 'tree',
-    markerColor: 'green'
+var marker2 = L.MakiMarkers.icon({
+    icon: 'park2',
+    color: '#3D9970',
+    size: 'l'
 });
 
-var marker3 = L.AwesomeMarkers.icon({
-    icon: 'building-o',
-    markerColor: 'red'
+var marker3 = L.MakiMarkers.icon({
+    icon: 'industrial',
+    color: '#FF4136',
+    size: 'l'
 });
 
-var marker4 = L.AwesomeMarkers.icon({
-    icon: 'coffee',
-    markerColor: 'red'
+var marker4 = L.MakiMarkers.icon({
+    icon: 'car',
+    color: '#001f3f',
+    size: 'l'
 });
 
-var marker5 = L.AwesomeMarkers.icon({
-    icon: 'coffee',
-    markerColor: 'red'
+var marker5 = L.MakiMarkers.icon({
+    icon: 'circle',
+    color: '#F012BE',
+    size: 'l'
 });
 
-var marker6 = L.AwesomeMarkers.icon({
-    icon: 'coffee',
-    markerColor: 'red'
+var marker6 = L.MakiMarkers.icon({
+    icon: 'wetland',
+    color: '#39CCCC',
+    size: 'l'
 });
 
-var marker7 = L.AwesomeMarkers.icon({
-    icon: 'coffee',
-    markerColor: 'red'
+var marker7 = L.MakiMarkers.icon({
+    icon: 'soccer',
+    color: '#FF851B',
+    size: 'l'
 });
 
-var marker8 = L.AwesomeMarkers.icon({
-    icon: 'coffee',
-    markerColor: 'red'
+var marker8 = L.MakiMarkers.icon({
+    icon: 'park',
+    color: '#2ECC40',
+    size: 'l'
 });
 
+var marker9 = L.MakiMarkers.icon({
+    icon: 'art-gallery',
+    color: '#85144b',
+    size: 'l'
+});
+
+var marker10 = L.MakiMarkers.icon({
+    icon: 'town-hall',
+    color: '#B10DC9',
+    size: 'l'
+});
+
+var marker11 = L.MakiMarkers.icon({
+    icon: 'cemetery',
+    color: '#111111',
+    size: 'l'
+});
 
 L.marker([41.40961103874989, 2.0174024933567725], {icon: marker1})
-    .bindPopup("<b>Casa de les Aigües / Molins de Rei</b><br /> El canal es va construir aprofitant les aigües del Rec Vell, que agafava les aigües del Llobregat al Papiol. En aquest edifici es controlava quanta aigua del anava al canal i quanta tornava al riu, marcant l’inici del canal. La infanta Luisa Carlota de Borbón va obrir les comportes.").addTo(map);
+    .bindPopup("<strong>Casa de les Aigües / Molins de Rei</strong><br /><img src='img/casa-aigues.jpg'><br />El canal es va construir aprofitant les aigües del Rec Vell, que agafava les aigües del Llobregat al Papiol.<br />En aquest edifici es controlava quanta aigua del anava al canal i quanta tornava al riu, marcant l’inici del canal. La infanta Luisa Carlota de Borbón va obrir les comportes.").addTo(map);
 
-L.marker([41.40351729938136, 2.027182289462256], {icon: marker2})
-    .bindPopup("<b>Carrer del General Castaños</b><br />El capità general de Catalunya, Francisco Javier Castaños, va ser un gran impulsor d’aquest canal. S’havia de dir “Canal de Castaños”, però es va acabar dedicant a la infanta Carlota perquè, aprofitant que visitava Barcelona, se la va convidar a inaugurar el canal tot i no estar acabat. A canvi, se li va dedicar un carrer. També hi havia l’ordre de plantar castanyers a la riba, tot i que no es va arribar a fer.").addTo(map);
+L.marker([41.408973, 2.016457], {icon: marker2})
+    .bindPopup("<strong>Carrer del General Castaños</strong><br />El capità general de Catalunya, Francisco Javier Castaños, va ser un gran impulsor d’aquest canal.<br />S’havia de dir “Canal de Castaños”, però es va acabar dedicant a la infanta Carlota perquè, aprofitant que visitava Barcelona, se la va convidar a inaugurar el canal tot i no estar acabat. A canvi, se li va dedicar un carrer. També hi havia l’ordre de plantar castanyers a la riba, tot i que no es va arribar a fer.").addTo(map);
 
-L.marker([41.39955601615708, 2.0330283274126733], {icon: marker3})
-    .bindPopup("<b>Avinguda Barcelona / Molins de Rei</b><br />Després de la cobertura que es va fer l’any (????), aquest és el primer punt on el canal veu la llum, a l’inici del polígon industrial el Pla. En bona part del polígon passa per dins de les fàbriques o als seus terrenys.").addTo(map);
+L.marker([41.403519, 2.027182], {icon: marker3})
+    .bindPopup("<strong>Avinguda Barcelona / Molins de Rei</strong><br />Després de la cobertura que es va fer l’any (????), aquest és el primer punt on el canal veu la llum, a l’inici del polígon industrial el Pla. En bona part del polígon passa per dins de les fàbriques o als seus terrenys.").addTo(map);
 
-L.marker([41.39720862932588, 2.035329974258218], {icon: marker4})
-    .bindPopup("<b>Pont de Can Capellans</b><br />I am a popup.").addTo(map);
+L.marker([41.400489, 2.031935], {icon: marker4})
+    .bindPopup("<strong>Pont de Can Capellans</strong><br />Un dels ponts més singulars, que permet el pas de vehicles a la part superior i el de persones per les escales.").addTo(map);
 
-L.marker([41.37628589811898, 2.0487785445566775], {icon: marker5})
-    .bindPopup("<b>Ciutat esportiva FC Barcelona</b><br />Aquest era un dels pocs punts on el canal encara es conservava, però la construcció de la ciutat esportiva del FC Barcelona ho va destruir.").addTo(map);
+L.marker([41.397207, 2.035329], {icon: marker5})
+    .bindPopup("<strong>Pont de Can Sant Feliu del Camí</strong><br />Des del tram on el canal es conserva tal i com era en un principi, l’arqueòloga del Centre d’Estudis del Baix Llobregat explica les seves característiques.").addTo(map);
 
-L.marker([41.35823979330778, 2.0846497816289085], {icon: marker6})
-    .bindPopup("<b>Ciutat esportiva FC Barcelona</b><br />I am a popup.").addTo(map);
+L.marker([41.374994, 2.051414], {icon: marker6})
+    .bindPopup("<strong>Salt de l’Erasme</strong><br />A partir d’aquest punt l’aigua del canal es desvia cap a sèquies menors fins a Can Trabal, a l’Hospitalet.").addTo(map);
 
-L.marker([41.36129181647807, 2.0921319431464958], {icon: marker7})
-    .bindPopup("<b>Parc de Can Mercader</b><br />I am a popup.").addTo(map);
+L.marker([41.357630, 2.085278], {icon: marker7})
+    .bindPopup("<strong>Parc de Can Mercader / Cornellà de Llobregat</strong><br />Gràcies a que la lluita veïnal va fer que Can Mercader fos una zona verda lliure d’edificacions, s’ha pogut conservar un pont del canal.").addTo(map);
 
 L.marker([41.352521887365626, 2.1521511626534315], {icon: marker8})
-    .bindPopup("<b>Pont Cornellà/Hospitalet</b><br />I am a popup.").addTo(map);
+    .bindPopup("<strong>Pont Cornellà / Hospitalet</strong><br />A pocs metres del pont de la Remunta hi ha un tros de canal que sí que s’ha preservat. L’ajuntament de Cornellà no vol construir en aquesta zona a diferència del de l’Hospitalet.").addTo(map);
+
+L.marker([41.359509, 2.095872], {icon: marker9})
+    .bindPopup("<strong>La Remunta / l’Hospitalet de Llobregat</strong><br />La plataforma “Protegim el canal de la infanta”, encapçalada per l’historiador Ireneu Castillo, va aconseguir que es preservés el pont i el canal que hi havia dins l’antiga caserna de la Remunta. Tot i això, els criteris per restaurar-lo han estat més estètics que històrics.").addTo(map);
+
+L.marker([41.357150, 2.067833], {icon: marker10})
+    .bindPopup("<strong>Can Trabal / L’Hospitalet</strong><br />L’últim punt per on ara mateix segueix circulant aigua. A partir de la ciutat esportiva de St. Joan Despí el canal principal ja no porta aigua i circula desviada per canals secundaris.").addTo(map);
+
+L.marker([41.353161, 2.155336], {icon: marker11})
+    .bindPopup("<strong>Cementiri de Montjuïc / Barcelona</strong><br />L’últim punt on es conserva el canal, per estar adossat al mur del cementiri de Montjuïc.").addTo(map);
