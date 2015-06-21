b2837504faa1a4b0114b6a75205f0072f5b09c98
Martín González <m@martingonzalez.net>
2015-06-21 17:46:10 +0200

    Clarify some icons

--- app/js/main.js
+++ app/js/main.js
@@ -26,14 +26,14 @@ map.scrollWheelZoom.disable();
 
 // Maki markers
 var marker1 = L.MakiMarkers.icon({
-    icon: 'water',
-    color: '#0074D9',
+    icon: 'farm',
+    color: '#39CCCC',
     size: 'l'
 });
 
 var marker2 = L.MakiMarkers.icon({
-    icon: 'park2',
-    color: '#3D9970',
+    icon: 'industrial',
+    color: '#FF4136',
     size: 'l'
 });
 
@@ -44,14 +44,14 @@ var marker3 = L.MakiMarkers.icon({
 });
 
 var marker4 = L.MakiMarkers.icon({
-    icon: 'car',
-    color: '#001f3f',
+    icon: 'circle',
+    color: '#0074D9',
     size: 'l'
 });
 
 var marker5 = L.MakiMarkers.icon({
     icon: 'circle',
-    color: '#F012BE',
+    color: '#0074D9',
     size: 'l'
 });
 
@@ -62,25 +62,25 @@ var marker6 = L.MakiMarkers.icon({
 });
 
 var marker7 = L.MakiMarkers.icon({
-    icon: 'soccer',
-    color: '#FF851B',
-    size: 'l'
-});
-
-var marker8 = L.MakiMarkers.icon({
     icon: 'park',
     color: '#2ECC40',
     size: 'l'
 });
 
+var marker8 = L.MakiMarkers.icon({
+    icon: 'industrial',
+    color: '#FF4136',
+    size: 'l'
+});
+
 var marker9 = L.MakiMarkers.icon({
-    icon: 'art-gallery',
-    color: '#85144b',
+    icon: 'circle',
+    color: '#0074D9',
     size: 'l'
 });
 
 var marker10 = L.MakiMarkers.icon({
-    icon: 'town-hall',
+    icon: 'soccer',
     color: '#B10DC9',
     size: 'l'
 });
@@ -92,10 +92,10 @@ var marker11 = L.MakiMarkers.icon({
 });
 
 L.marker([41.40961103874989, 2.0174024933567725], {icon: marker1})
-    .bindPopup("<strong>Casa de les Aigües / Molins de Rei</strong><br /><img src='img/casa-aigues.jpg'><br />El canal es va construir aprofitant les aigües del Rec Vell, que agafava les aigües del Llobregat al Papiol.<br />En aquest edifici es controlava quanta aigua del anava al canal i quanta tornava al riu, marcant l’inici del canal. La infanta Luisa Carlota de Borbón va obrir les comportes.").addTo(map);
+    .bindPopup("<strong>Casa de les Aigües / Molins de Rei</strong><br /><img src='img/casa-aigues.jpg'><br />El canal es va construir aprofitant les aigües del Rec Vell, que agafava les aigües del Llobregat al Papiol. En aquest edifici es controlava quanta aigua del anava al canal i quanta tornava al riu, marcant l’inici del canal. La infanta Luisa Carlota de Borbón va obrir les comportes.").addTo(map);
 
 L.marker([41.408973, 2.016457], {icon: marker2})
-    .bindPopup("<strong>Carrer del General Castaños</strong><br />El capità general de Catalunya, Francisco Javier Castaños, va ser un gran impulsor d’aquest canal.<br />S’havia de dir “Canal de Castaños”, però es va acabar dedicant a la infanta Carlota perquè, aprofitant que visitava Barcelona, se la va convidar a inaugurar el canal tot i no estar acabat. A canvi, se li va dedicar un carrer. També hi havia l’ordre de plantar castanyers a la riba, tot i que no es va arribar a fer.").addTo(map);
+    .bindPopup("<strong>Carrer del General Castaños</strong><br />El capità general de Catalunya, Francisco Javier Castaños, va ser un gran impulsor d’aquest canal.<br /> S’havia de dir “Canal de Castaños”, però es va acabar dedicant a la infanta Carlota perquè, aprofitant que visitava Barcelona, se la va convidar a inaugurar el canal tot i no estar acabat. A canvi, se li va dedicar un carrer. També hi havia l’ordre de plantar castanyers a la riba, tot i que no es va arribar a fer.").addTo(map);
 
 L.marker([41.403519, 2.027182], {icon: marker3})
     .bindPopup("<strong>Avinguda Barcelona / Molins de Rei</strong><br />Després de la cobertura que es va fer l’any (????), aquest és el primer punt on el canal veu la llum, a l’inici del polígon industrial el Pla. En bona part del polígon passa per dins de les fàbriques o als seus terrenys.").addTo(map);
@@ -112,8 +112,8 @@ L.marker([41.374994, 2.051414], {icon: marker6})
 L.marker([41.357630, 2.085278], {icon: marker7})
     .bindPopup("<strong>Parc de Can Mercader / Cornellà de Llobregat</strong><br />Gràcies a que la lluita veïnal va fer que Can Mercader fos una zona verda lliure d’edificacions, s’ha pogut conservar un pont del canal.").addTo(map);
 
-L.marker([41.352521887365626, 2.1521511626534315], {icon: marker8})
-    .bindPopup("<strong>Pont Cornellà / Hospitalet</strong><br />A pocs metres del pont de la Remunta hi ha un tros de canal que sí que s’ha preservat. L’ajuntament de Cornellà no vol construir en aquesta zona a diferència del de l’Hospitalet.").addTo(map);
+L.marker([41.361226, 2.090673], {icon: marker8})
+    .bindPopup("<strong>Rambla Solanes / Hospitalet</strong><br />Fins fa ben poc la Remunta tenia un aspecte similar al d’aquesta zona, que es troba a molt pocs metres però en terme municipal de Cornellà, sense interès a urbanitzar la zona.").addTo(map);
 
 L.marker([41.359509, 2.095872], {icon: marker9})
     .bindPopup("<strong>La Remunta / l’Hospitalet de Llobregat</strong><br />La plataforma “Protegim el canal de la infanta”, encapçalada per l’historiador Ireneu Castillo, va aconseguir que es preservés el pont i el canal que hi havia dins l’antiga caserna de la Remunta. Tot i això, els criteris per restaurar-lo han estat més estètics que històrics.").addTo(map);
@@ -122,4 +122,4 @@ L.marker([41.357150, 2.067833], {icon: marker10})
     .bindPopup("<strong>Can Trabal / L’Hospitalet</strong><br />L’últim punt per on ara mateix segueix circulant aigua. A partir de la ciutat esportiva de St. Joan Despí el canal principal ja no porta aigua i circula desviada per canals secundaris.").addTo(map);
 
 L.marker([41.353161, 2.155336], {icon: marker11})
-    .bindPopup("<strong>Cementiri de Montjuïc / Barcelona</strong><br />L’últim punt on es conserva el canal, per estar adossat al mur del cementiri de Montjuïc.").addTo(map);
+    .bindPopup("<strong>Cementiri de Montjuïc / Barcelona</strong><br />L’últim punt on es conserva el canal, per estar adossat al mur del cementiri de Montjuïc. Desembocava als peus del far de Montjuïc, però l’ampliació del port i la construcció d’infraestructures ho han esborrat.").addTo(map);
